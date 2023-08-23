2241652ce65ba8f4ef477be23d31cc0606fd439b
Karl George <karlgdev@gmail.com>
2023-08-23 12:21:21 +0200

    neew folder to fix hot reload

---
+++ components/Library.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { TbPlaylist } from 'react-icons/tb';
+import { AiOutlinePlus } from 'react-icons/ai';
+
+interface LibraryProps {}
+
+function Library({}: LibraryProps) {
+  const onClick = () => {
+    // Handle upload later
+  };
+  return (
+    <div className='flex flex-col'>
+      <div className='flex items-center justify-between px-5 pt-4'>
+        <div className='inline-flex items-center gap-x-2'>
+          <TbPlaylist size={26} className='text-neutral-400' />
+          <p className='text-neutral-400 font-medium text-md'>Your Library</p>
+        </div>
+        <AiOutlinePlus
+          onClick={onClick}
+          size={30}
+          className='text-neutral-400 cursor-pointer hover:text-white transition'
+        />
+      </div>
+      <div className='flex flex-col gap-y-2 mt-3 px-3'>List of Songs!</div>
+    </div>
+  );
+}
+
+export default Library;
---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import AuthModal from '@/components/AuthModal';
+
+interface ModalProviderProps {}
+
+const ModalProvider: React.FC<ModalProviderProps> = ({}) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <AuthModal />
+    </>
+  );
+};
+
+export default ModalProvider;
