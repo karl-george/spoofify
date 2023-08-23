f2a90f92ad3db8dff2a0ebd879c6c7e69a58a355
Karl George <karlgdev@gmail.com>
2023-08-23 12:33:54 +0200

    add twmerge

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import { BiSearch } from 'react-icons/bi';
 import Box from './Box';
 import SidebarItem from './SidebarItem';
 import Library from './Library';
+import { twMerge } from 'tailwind-merge';
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -33,7 +34,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
     [pathname]
   );
   return (
-    <div className='flex h-full'>
+    <div className={twMerge('flex h-full')}>
       <div className='hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2'>
         <Box>
           <div className='flex flex-col gap-y-4 px-5 py-4'>
