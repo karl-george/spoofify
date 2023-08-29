53575a6ecff697f953df50a1d7ff54d47926fcb2
Karl George <karlgdev@gmail.com>
2023-08-29 15:11:25 +0200

    add player component

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import SupabaseProvider from '@/providers/SupabaseProvider';
 import './globals.css';
 import ToasterProvider from '@/providers/ToasterProvider';
 import getSongsByUserId from '@/actions/getSongsByUserId';
+import Player from '@/components/Player';
 
 const font = Figtree({ subsets: ['latin'] });
 
@@ -33,6 +34,7 @@ export default async function RootLayout({
           <UserProvider>
             <ModalProvider />
             <Sidebar songs={userSongs}>{children}</Sidebar>
+            <Player />
           </UserProvider>
         </SupabaseProvider>
       </body>
