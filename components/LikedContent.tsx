ec7979023ee74c6f5aa5ffe2f725b3f631ea737f
Karl George <karlgdev@gmail.com>
2023-08-29 14:51:10 +0200

    Adding showing liked songs

---
+++ components/LikedContent.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { useUser } from '@/hooks/useUser';
+import { Song } from '@/types';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import MediaItem from './MediaItem';
+import LikeButton from './LikeButton';
+
+interface LikedContentProps {
+  songs: Song[];
+}
+
+function LikedContent({ songs }: LikedContentProps) {
+  const router = useRouter();
+  const { user, isLoading } = useUser();
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace('/');
+    }
+  }, [isLoading, user, router]);
+
+  if (songs.length === 0) {
+    return (
+      <div className='flex flex-col gap-y-2 w-full px-6 text-neutral-400'>
+        No liked songs
+      </div>
+    );
+  }
+
+  return (
+    <div className='flex flex-col gap-y-2 w-full p-6'>
+      {songs.map((song) => (
+        <div key={song.id} className='flex items-center gap-x-4 w-full'>
+          <div className='flex-1'>
+            <MediaItem onClick={() => {}} data={song} />
+          </div>
+          <LikeButton songId={song.id} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default LikedContent;
