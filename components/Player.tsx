8d2a904c6a48616c0787029693e1a712452a52bb
Karl George <karlgdev@gmail.com>
2023-08-29 15:11:19 +0200

    Working on player logic

---
+++ components/Player.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import useGetSongById from '@/actions/useGetSongById';
+import useLoadSongUrl from '@/hooks/useLoadSongUrl';
+import usePlayer from '@/hooks/usePlayer';
+
+interface PlayerProps {}
+
+function Player({}: PlayerProps) {
+  const player = usePlayer();
+  const { song } = useGetSongById(player.activeId);
+
+  const songUrl = useLoadSongUrl(song!);
+
+  if (!song || songUrl || player.activeId) {
+    return null;
+  }
+
+  return (
+    <div className='fixed bottom-0 bg-black w-full py-2 px-4 h-[80px]'>
+      Player
+    </div>
+  );
+}
+
+export default Player;
