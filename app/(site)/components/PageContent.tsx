809510aa285c00feda87529ec491c5742ba74f5a
Karl George <karlgdev@gmail.com>
2023-08-29 15:20:55 +0200

    add on play onclick

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import SongItem from '@/components/SongItem';
+import useOnPlay from '@/hooks/useOnPlay';
 import { Song } from '@/types';
 
 interface PageContentProps {
@@ -8,6 +9,8 @@ interface PageContentProps {
 }
 
 function PageContent({ songs }: PageContentProps) {
+  const onPlay = useOnPlay(songs);
+
   if (songs.length === 0) {
     <div className='mt-4 text-neutral-400'>No songs available</div>;
   }
@@ -15,7 +18,11 @@ function PageContent({ songs }: PageContentProps) {
   return (
     <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4'>
       {songs.map((song) => (
-        <SongItem key={song.id} onClick={() => {}} data={song} />
+        <SongItem
+          key={song.id}
+          onClick={(id: string) => onPlay(id)}
+          data={song}
+        />
       ))}
     </div>
   );
