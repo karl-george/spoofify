608081f96a5fa738d43128abdfca9db438e23873
Karl George <karlgdev@gmail.com>
2023-08-30 11:52:24 +0200

    Designing volume slider

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,9 +1,31 @@
 'use client';
 
-interface SliderProps {}
+import * as RadixSlider from '@radix-ui/react-slider';
 
-function Slider({}: SliderProps) {
-  return <div>Slider</div>;
+interface SliderProps {
+  value?: number;
+  onChange?: (value: number) => void;
+}
+
+function Slider({ value = 1, onChange }: SliderProps) {
+  const handleChange = (newValue: number[]) => {
+    onChange?.(newValue[0]);
+  };
+  return (
+    <RadixSlider.Root
+      className='relative flex items-center select-none touch-none w-full h-10'
+      defaultValue={[1]}
+      value={[value]}
+      onValueChange={handleChange}
+      max={1}
+      step={0.1}
+      aria-label='volume'
+    >
+      <RadixSlider.Track className='bg-neutral-600 relative grow rounded-full h-[3px]'>
+        <RadixSlider.Range className='absolute bg-white rounded-full h-full' />
+      </RadixSlider.Track>
+    </RadixSlider.Root>
+  );
 }
 
 export default Slider;
