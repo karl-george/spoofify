1c806004d1175e7469e7a38ccb5858912ea73dea
Karl George <karlgdev@gmail.com>
2023-08-30 12:12:22 +0200

    changing height when player is active

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,6 +9,7 @@ import SidebarItem from './SidebarItem';
 import Library from './Library';
 import { twMerge } from 'tailwind-merge';
 import { Song } from '@/types';
+import usePlayer from '@/hooks/usePlayer';
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -17,7 +18,7 @@ interface SidebarProps {
 
 const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
   const pathname = usePathname();
-
+  const player = usePlayer();
   const routes = useMemo(
     () => [
       {
@@ -36,7 +37,12 @@ const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
     [pathname]
   );
   return (
-    <div className={twMerge('flex h-full')}>
+    <div
+      className={twMerge(
+        'flex h-full',
+        player.activeId && 'h-[calc(100%-80px)]'
+      )}
+    >
       <div className='hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2'>
         <Box>
           <div className='flex flex-col gap-y-4 px-5 py-4'>
